ca1b71abd9484e003dabb5cf8370d64afaaabeca
nikita5677 <67727961+nikita5677@users.noreply.github.com>
2020-07-08 18:20:49 +0530

    Delete 3 split.py

--- 3 split.py
+++
@@ -1,4 +0,0 @@
-for _set in ['train', 'val', 'test']:
-    n_normal = len(os.listdir(input_path + _set + '/NORMAL'))
-    n_infect = len(os.listdir(input_path + _set + '/PNEUMONIA'))
-    print('Set: {}, normal images: {}, pneumonia images: {}'.format(_set, n_normal, n_infect))
