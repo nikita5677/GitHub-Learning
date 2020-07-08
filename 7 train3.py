81cf325f2e74b8406cba66be6d56b20c5b245185
nikita5677 <67727961+nikita5677@users.noreply.github.com>
2020-07-08 18:22:08 +0530

    Delete 7 train3.py

--- 7 train3.py
+++
@@ -1,4 +0,0 @@
-hist = model.fit_generator(
-           train_gen, steps_per_epoch=train_gen.samples // batch_size, 
-           epochs=epochs, validation_data=test_gen, 
-           validation_steps=test_gen.samples // batch_size, callbacks=[checkpoint, lr_reduce])
